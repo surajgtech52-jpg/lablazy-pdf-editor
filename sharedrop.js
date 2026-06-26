16c0382769c604a6dadcb787a622943ba05b90b1
Suraj Gupta <269257015+surajgtech52-jpg@users.noreply.github.com>
2026-06-27 01:28:44 +0530

    Update sharedrop.js

--- sharedrop.js
+++ sharedrop.js
@@ -1863,7 +1863,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // LOBBY SIGNALING MATCHMAKER (WEBSOCKETS)
     // ==========================================
    const USE_LOCAL_SERVER = window.location.hostname === 'localhost'; // Set to true only if running Node serv
-    const SIGNALING_HOST = USE_LOCAL_SERVER ? 'localhost:8080' : 'lablazy-signaling-server.onrender.com';
+   const SIGNALING_HOST = USE_LOCAL_SERVER ? 'localhost:8080' : 'lablazy-signaling-server.onrender.com';
 
     let signalingSocket = null;
     let heartbeatIntervalId = null;
